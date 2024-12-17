ed06e9ac2f8322adf2ade68317e80b2e43d8fe4a
taayyohh <milesxbridges@gmail.com>
2024-12-17 08:39:32 -0500

    update deployment

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -14,15 +14,16 @@ export interface PonderAddresses {
 // Contract addresses for each supported chain
 export const PONDER_ADDRESSES: Record<ChainId, PonderAddresses> = {
   25925: {
-    factory: "0x9Ca89d7531a4A467729e0C8858e5433a9Df4D0Cb",
-    router: "0xD1e1b2Db5BeB99462787FB0921178F1A23560410",
-    masterChef: "0xA97071b70cA4dFe81Edb0691d81354a2e833A9ae",
-    launcher: "0x82A69Bd8081D5577997210d0a57A6c4c0B6e4AF8",
-    oracle: "0x5dAB1F58411e21c8D8Fc9CA4502d9bC635A8692B",
-    kkubUnwrapper: "0x962047A3884aE32cFa8bEA12785F6B44A85aE1C5",
-    ponderToken: "0xb7eDD1804D10aa505aFF7655E86b40A944168045",
+    factory: "0x9f9F5F722F5AeCf8e0968b4bC4ff641660e2A9ac",
+    router: "0x8F3eD06ca173c09B175cf164e991Bc6325a8F298",
+    masterChef: "0x95c491a4C1AE977Ed778CF22042B5050Dc1D9988",
+    launcher: "0xfAe8bf1385A1c8C603730dA5c326B17C392Ad51E",
+    oracle: "0xae5c8eDE1E9Db00419925eE675257a31266a6022",
+    kkubUnwrapper: "0xDb5cCF0449Fff51603D2aa69fa3F19be94262858",
+    ponderToken: "0xA3f5c38D0C1F2D65381562a66F6C8391cecfa549",
   },
 } as const;
+
 
 // KKUB addresses for each chain
 export const KKUB_ADDRESSES: Record<ChainId, Address> = {
