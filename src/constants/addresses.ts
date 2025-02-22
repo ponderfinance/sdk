69a9f271342812c01089f98d3c34b0f24b05715f
taayyohh <milesxbridges@gmail.com>
2025-02-22 16:08:18 -0500

    update addresses

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -16,15 +16,15 @@ export interface PonderAddresses {
 // Contract addresses for each supported chain
 export const PONDER_ADDRESSES: Record<ChainId, PonderAddresses> = {
   25925: {
-    factory: "0x07Ac7Fb45a48699c9f3eb1bd02C9e03F21e3c4eD",
-    router: "0x2122754635236E6191BfA54241eC3f41728740F6",
-    masterChef: "0xa129044Ad5A38BD173BF11a57f8ceE271a606250",
-    launcher: "0xCC1687173299804Abeb7267686a970D6Bf4E04a3",
-    oracle: "0x0Ba4AA312F8D708d9ae4f456e0bdFde0141080c9",
-    kkubUnwrapper: "0x38B756c8925af4F707559aA638006e819a66619b",
-    ponderToken: "0xcFfC775E261d26595C9ec17Ad36cc2783785DD84",
-    staking: "0x19a6595012A7Cd65D851198764852EaC170F4B27",
-    feeDistributor: "0x55C5152DbC01788FcF9B09576f46AD8Dca4207e1",
+    factory: "0x9285eca11bfBb2Ef29fc746Fee36Aa71AFF89708",
+    router: "0xbb5eb065862AD7279D8ca2bb383c298a86aEC5cA",
+    masterChef: "0x9e69fBa912900739Ae48D742BB1Ffc5ff3FfBcF0",
+    launcher: "0x092E2922Cd4519cf5B53C380A2ABFD62883fD1CE",
+    oracle: "0xad3cA3a10a72310936D97dD673c4e9cE97D5B7b4",
+    kkubUnwrapper: "0x19b49138B65883d694746eA2Ca8149Aa79eb319A",
+    ponderToken: "0x66Ecc3cbFAb2c9Eb8bFe91b4fE65F57129F4d164",
+    staking: "0x6e0559a71760CBd2F9d31aC0D4681fa49fa65b52",
+    feeDistributor: "0xE6bF34591D9e60d4aB5833fB885206D6967f811e",
   },
 } as const;
 
